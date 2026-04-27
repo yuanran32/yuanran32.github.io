31356d9733a5290ee69145cabaa6e5b2fa0fdd90
yuanran <16614519+yuanran687@user.noreply.gitee.com>
2026-04-27 21:58:04 +0800

    部署问题再修改

--- astro.config.mjs
+++ astro.config.mjs
@@ -27,7 +27,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 // https://astro.build/config
 export default defineConfig({
 	site: "https://yuanran32.github.io",
-	base: "",
+	base: "/elowen/",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
